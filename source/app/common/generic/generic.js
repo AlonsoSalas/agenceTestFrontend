17998b465b6f9dd6877612a4bbca2121b6be2e8b
Alonso Salas <alosalasv@gmail.com>
2017-11-27 19:31:51 -0400

    --amend

--- source/app/common/generic/generic.js
+++ source/app/common/generic/generic.js
@@ -5,7 +5,8 @@ import ApiService from './api.service';
 let genericModule = angular.module('generic', [])
 
 .service('ApiService', ApiService )
-.constant('ServiceBaseUrl', 'http://127.0.0.1:8000/api' )
+// .constant('ServiceBaseUrl', 'http://127.0.0.1:8000/api' )
+.constant('ServiceBaseUrl', 'https://agence-test-alonso.herokuapp.com/api' )
 
 .name;
 
